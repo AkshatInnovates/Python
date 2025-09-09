a82e7377d3ab41aaf0455bfcf5a7d1e888847bef
Akshat Jain <145150367+AkshatInnovates@users.noreply.github.com>
2025-09-09 09:44:56 +0000

    loops done

---
+++ 10 loop problems/10_problem.py
@@ -0,0 +1,12 @@
+# Exponential Backoff
+# Implement an exponential backoff strategy that doubles the wait time between retires, starting from 1 second,but stops,
+#  after 5 retires.
+import time
+wait_time = 1
+max_retires = 5
+attempts = 0
+while attempts <max_retires:
+    print("Attempt",attempts+1,"-wait time",wait_time)
+    time.sleep(wait_time)
+    wait_time *=2
+    attempts +=1
