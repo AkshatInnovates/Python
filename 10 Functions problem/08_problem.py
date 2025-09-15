5f724e12b3997719b4a5198d561fd4a151d1f674
Akshat Jain <145150367+AkshatInnovates@users.noreply.github.com>
2025-09-15 10:43:02 +0000

    done

--- 10 Functions problem/08_problem.py
+++ 10 Functions problem/08_problem.py
@@ -1,2 +1,7 @@
 # Function with "Kwargs"
 # Create a function that accepts any number  of keywords arguments and prints them in the format key:value.
+def print_kwargs(name,power):
+    print('name',name,'power',power)
+
+
+print_kwargs(name="Shaktiman",power="Lazer")
